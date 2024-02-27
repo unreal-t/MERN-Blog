e9f95335621fa5d2b5ec22e05d33bf9f83c33292
unreal-t <69441250+unreal-t@users.noreply.github.com>
2024-02-27 15:22:55 +0700

    changes made for tailwinds config

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex bg-slate-500 w-300 h-400 bg-center justify-center text-center items-center text-8xl p-3">Home</div>
   )
 }
