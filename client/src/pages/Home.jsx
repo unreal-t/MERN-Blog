42a3187a238c9707e287231f8831e47637984678
unreal-t <69441250+unreal-t@users.noreply.github.com>
2024-02-27 18:11:23 +0700

    Home jsx Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="flex bg-slate-500 w-300 h-400 bg-center justify-center text-center items-center text-8xl p-3">Home</div>
+    <div className="flex-auto bg-slate-200 w-600 h-400 justify-center text-center items-center text-6xl p-3">Home</div>
   )
 }
