d8e328187303d8fd87ecb5c0ba85303b5375b384
unreal-t <69441250+unreal-t@users.noreply.github.com>
2024-03-15 15:33:42 +0700

    connect to thee database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import { mongoose } from 'mongoose';
+import dotenv from 'dotenv';
+// import cors from 'cors';    
+// import postRoutes from './routes/posts.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.info('database connected');
+}).catch((err) => {
+    console.info(err.message);
+});;
 
 const app = express();
 
